cb8995c3b6f03fba5ef2cb7eaef2692317277640
Arseniy Bannikov <uma7op@gmail.com>
2025-12-10 20:57:29 +0300

    Removed main

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,4 @@
 module Main where
 
-import ParseProblem (Form(..))
-import qualified Clausify as C
-import FormulaRepr (flatClauseToFormula, implClauseToFormula)
-
-formula :: Form
-formula = Atom "a" :=>: (Atom "b" :=>: Atom "c")
-
-(flat, impl) = C.clausify [formula]
-flatFormulae = map flatClauseToFormula flat
-implFormulae = map implClauseToFormula impl
-
 main :: IO ()
-main = do
-  print flatFormulae
-  print implFormulae
+main = putStrLn "Hello"
